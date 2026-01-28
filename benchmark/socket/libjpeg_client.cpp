a6e0a88156b4634ca6cc488a1b3e1f054a81e20b
nitinm25 <nitinmathai25@gmail.com>
2026-01-28 18:58:32 +0000

    libjpeg ipc read_jpeg

--- benchmark/socket/libjpeg_client.cpp
+++ benchmark/socket/libjpeg_client.cpp
@@ -48,6 +48,70 @@ void ipc_jpeg_create_decompress(int server_fd, j_decompress_ptr cinfo) {
     socket_recv(server_fd, (unsigned char*)&ack, sizeof(ack));
 }
 
+void ipc_jpeg_mem_src(int server_fd, j_decompress_ptr cinfo, unsigned char* buffer, unsigned long size) {
+    uint32_t ipc_call_num = IPC_JPEG_MEM_SRC;
+    socket_send(server_fd, (unsigned char*)&ipc_call_num, sizeof(ipc_call_num));
+
+    socket_send(server_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    socket_send(server_fd, (unsigned char*)&buffer, sizeof(buffer));
+    socket_send(server_fd, (unsigned char*)&size, sizeof(size));
+
+    uint32_t ack;
+    socket_recv(server_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void ipc_jpeg_read_header(int server_fd, j_decompress_ptr cinfo, boolean require_image) {
+    uint32_t ipc_call_num = IPC_JPEG_READ_HEADER;
+    socket_send(server_fd, (unsigned char*)&ipc_call_num, sizeof(ipc_call_num));
+
+    socket_send(server_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    socket_send(server_fd, (unsigned char*)&require_image, sizeof(require_image));
+
+    uint32_t ack;
+    socket_recv(server_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void ipc_jpeg_start_decompress(int server_fd, j_decompress_ptr cinfo) {
+    uint32_t ipc_call_num = IPC_JPEG_START_DECOMPRESS;
+    socket_send(server_fd, (unsigned char*)&ipc_call_num, sizeof(ipc_call_num));
+
+    socket_send(server_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+
+    uint32_t ack;
+    socket_recv(server_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void ipc_jpeg_read_scanlines(int server_fd, j_decompress_ptr cinfo, JSAMPARRAY buffer, JDIMENSION max_lines) {
+    uint32_t ipc_call_num = IPC_JPEG_READ_SCANLINES;
+    socket_send(server_fd, (unsigned char*)&ipc_call_num, sizeof(ipc_call_num));
+
+    socket_send(server_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    socket_send(server_fd, (unsigned char*)&buffer, sizeof(buffer));
+    socket_send(server_fd, (unsigned char*)&max_lines, sizeof(max_lines));
+
+    uint32_t ack;
+    socket_recv(server_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void ipc_jpeg_finish_decompress(int server_fd, j_decompress_ptr cinfo) {
+    uint32_t ipc_call_num = IPC_JPEG_FINISH_DECOMPRESS;
+    socket_send(server_fd, (unsigned char*)&ipc_call_num, sizeof(ipc_call_num));
+
+    socket_send(server_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+
+    uint32_t ack;
+    socket_recv(server_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void ipc_jpeg_destroy_decompress(int server_fd, j_decompress_ptr cinfo) {
+    uint32_t ipc_call_num = IPC_JPEG_DESTROY_DECOMPRESS;
+    socket_send(server_fd, (unsigned char*)&ipc_call_num, sizeof(ipc_call_num));
+
+    socket_send(server_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+
+    uint32_t ack;
+    socket_recv(server_fd, (unsigned char*)&ack, sizeof(ack));
+}
 
 void ipc_terminate(int server_fd) {
     uint32_t ipc_call_num = IPC_TERMINATE;
@@ -56,6 +120,10 @@ void ipc_terminate(int server_fd) {
 
 
 struct jpeg_parsed_data read_jpeg(int server_fd, mspace shared_heap, unsigned char *fileBuff, unsigned long fsize) {
+    // Libjpeg uses a pointer to the input buffer, copy to shared memory so it's accessible on the server
+    unsigned char* shared_input = (unsigned char*)mspace_malloc(shared_heap, fsize);
+    memcpy(shared_input, fileBuff, fsize);
+
     struct jpeg_parsed_data ret = {0};
 
     struct jpeg_decompress_struct* cinfo = (struct jpeg_decompress_struct*)mspace_malloc(shared_heap, sizeof(struct jpeg_decompress_struct));
@@ -68,6 +136,34 @@ struct jpeg_parsed_data read_jpeg(int server_fd, mspace shared_heap, unsigned ch
     jerr->error_exit = my_error_exit;
 
     ipc_jpeg_create_decompress(server_fd, cinfo);
+
+    ipc_jpeg_mem_src(server_fd, cinfo, shared_input, fsize);
+    ipc_jpeg_read_header(server_fd, cinfo, TRUE);
+    ipc_jpeg_start_decompress(server_fd, cinfo);
+
+    int row_stride = cinfo->output_width * cinfo->output_components;
+
+    ret.image_height = cinfo->output_height;
+    ret.image_width = cinfo->output_width;
+    ret.image_buffer_size = ret.image_width * ret.image_height * 3 * sizeof(JSAMPLE);
+    ret.image_buffer = (JSAMPLE*)mspace_malloc(shared_heap, ret.image_buffer_size);
+
+    RELEASE_ASSERT(ret.image_buffer, "Memory alloc failure");
+
+    int curr_image_row = 0;
+
+    // Allocate array pointer in shared memory for jpeg_read_scanlines
+    JSAMPLE** row_pointer = (JSAMPLE**)mspace_malloc(shared_heap, sizeof(JSAMPLE*));
+
+    while (cinfo->output_scanline < cinfo->output_height) {
+        JSAMPLE* target = &(ret.image_buffer[curr_image_row * row_stride]);
+        *row_pointer = target;
+        ipc_jpeg_read_scanlines(server_fd, cinfo, row_pointer, 1);
+        curr_image_row++;
+    }
+
+    ipc_jpeg_finish_decompress(server_fd, cinfo);
+    ipc_jpeg_destroy_decompress(server_fd, cinfo);
 
     return ret;
 }
--- benchmark/socket/libjpeg_server.cpp
+++ benchmark/socket/libjpeg_server.cpp
@@ -18,16 +18,92 @@ void handle_ipc_jpeg_create_decompress(int client_fd) {
     
     jpeg_create_decompress(cinfo);
     
-    // No return value, just send ACK
+    uint32_t ack = 1;
+    socket_send(client_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void handle_ipc_jpeg_mem_src(int client_fd) {
+    j_decompress_ptr cinfo;
+    unsigned char* buffer;
+    unsigned long size;
+    
+    socket_recv(client_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    socket_recv(client_fd, (unsigned char*)&buffer, sizeof(buffer));
+    socket_recv(client_fd, (unsigned char*)&size, sizeof(size));
+    
+    jpeg_mem_src(cinfo, buffer, size);
+    
+    uint32_t ack = 1;
+    socket_send(client_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void handle_ipc_jpeg_read_header(int client_fd) {
+    j_decompress_ptr cinfo;
+    boolean require_image;
+    
+    socket_recv(client_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    socket_recv(client_fd, (unsigned char*)&require_image, sizeof(require_image));
+    
+    jpeg_read_header(cinfo, require_image);
+    
+    uint32_t ack = 1;
+    socket_send(client_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void handle_ipc_jpeg_start_decompress(int client_fd) {
+    j_decompress_ptr cinfo;
+    socket_recv(client_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    
+    jpeg_start_decompress(cinfo);
+    
+    uint32_t ack = 1;
+    socket_send(client_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void handle_ipc_jpeg_read_scanlines(int client_fd) {
+    j_decompress_ptr cinfo;
+    JSAMPARRAY buffer;
+    JDIMENSION max_lines;
+    
+    socket_recv(client_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    socket_recv(client_fd, (unsigned char*)&buffer, sizeof(buffer));
+    socket_recv(client_fd, (unsigned char*)&max_lines, sizeof(max_lines));
+    
+    jpeg_read_scanlines(cinfo, buffer, max_lines);
+    
+    uint32_t ack = 1;
+    socket_send(client_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void handle_ipc_jpeg_finish_decompress(int client_fd) {
+    j_decompress_ptr cinfo;
+    socket_recv(client_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    
+    jpeg_finish_decompress(cinfo);
+    
+    uint32_t ack = 1;
+    socket_send(client_fd, (unsigned char*)&ack, sizeof(ack));
+}
+
+void handle_ipc_jpeg_destroy_decompress(int client_fd) {
+    j_decompress_ptr cinfo;
+    socket_recv(client_fd, (unsigned char*)&cinfo, sizeof(cinfo));
+    
+    jpeg_destroy_decompress(cinfo);
+    
     uint32_t ack = 1;
     socket_send(client_fd, (unsigned char*)&ack, sizeof(ack));
 }
 
 void server_run(int client_fd) {
+    int ipc_counter = 0;
+
     // Server loop
     while(true) {
         uint32_t cmd;
         socket_recv(client_fd, (unsigned char*)&cmd, sizeof(cmd));
+
+        ipc_counter++;
 
         switch(cmd) {
             case IPC_JPEG_STD_ERROR:
@@ -38,8 +114,32 @@ void server_run(int client_fd) {
                 handle_ipc_jpeg_create_decompress(client_fd);
                 std::cout << "JPEG CREATE DECOMPRESS" << std::endl;
                 break;
+            case IPC_JPEG_MEM_SRC:
+                handle_ipc_jpeg_mem_src(client_fd);
+                std::cout << "JPEG MEM SRC" << std::endl;
+                break;
+            case IPC_JPEG_READ_HEADER:
+                handle_ipc_jpeg_read_header(client_fd);
+                std::cout << "JPEG READ HEADER" << std::endl;
+                break;
+            case IPC_JPEG_START_DECOMPRESS:
+                handle_ipc_jpeg_start_decompress(client_fd);
+                std::cout << "JPEG START DECOMPRESS" << std::endl;
+                break;
+            case IPC_JPEG_READ_SCANLINES:
+                handle_ipc_jpeg_read_scanlines(client_fd);
+                // std::cout << "JPEG READ SCANLINES" << std::endl;
+                break;
+            case IPC_JPEG_FINISH_DECOMPRESS:
+                handle_ipc_jpeg_finish_decompress(client_fd);
+                std::cout << "JPEG FINISH DECOMPRESS" << std::endl;
+                break;
+            case IPC_JPEG_DESTROY_DECOMPRESS:
+                handle_ipc_jpeg_destroy_decompress(client_fd);
+                std::cout << "JPEG DESTROY DECOMPRESS" << std::endl;
+                break;
             case IPC_TERMINATE:
-                std::cout << "JPEG TERMINATE" << std::endl;
+                std::cout << "[JPEG TERMINATE] IPC count: " << ipc_counter << std::endl;
                 return;
         }
     }
